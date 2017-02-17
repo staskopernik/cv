8f9d07b5b264dfc6d09b0e1cc03227edbc06c8ed
Mykhailo <staskopernik@gmail.com>
2017-02-17 18:42:23 +0000

    fix bag

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -13,11 +13,10 @@ $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
 
-// Create the email and send the message
-$to = 'stas_kopernik@ukr.net'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'staskopernik@gmail.com'; 
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: stas_kopernik@ukr.net\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: staskopernik@gmail.com\n"; 
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
